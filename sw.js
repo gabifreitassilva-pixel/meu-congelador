18e976f616dd85c3e9fc489ed83e56241c7666de
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-03-11 13:25:57 -0300

    Add basic service worker for caching assets
    Implement basic service worker for PWA caching.

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Service Worker básico para PWA - "Gera" o app e salva os dados no aparelho
+var cacheName = 'meu-congelador-app-v14';
+var filesToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'app-icon.png' // Lembre-se de colocar as imagens cortadas nas dimensões corretas aqui se mudar os nomes
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
